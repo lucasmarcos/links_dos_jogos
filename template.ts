27912ceb4fb355c9cb3e1e374a49f8fc0c458c2a
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2026-03-31 20:20:51 -0300

    template

--- template.ts
+++ template.ts
@@ -14,39 +14,38 @@ type GameCardProps = {
   original: string;
 };
 
-export const getTemplate = ({ styles, linksHtml, year, author }: TemplateProps): string => html`
-<!DOCTYPE html>
+export const getTemplate = ({ styles, linksHtml, year, author }: TemplateProps): string => html`<!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Casa da Paz - Lista de Jogos</title>
-    <style>
-        ${styles}
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Casa da Paz - Lista de Jogos</title>
+  <style>
+    ${styles}
+  </style>
 </head>
 <body>
-    <header>
-        <div class="emoji-header">🎮</div>
-        <h1>Casa da Paz</h1>
-        <p>Links para Jogos Online</p>
-    </header>
-    <main class="container">
-        ${linksHtml}
-    </main>
-    <footer>
-        &copy; ${year} ${author}
-    </footer>
+  <header>
+    <div class="emoji-header">🎮</div>
+    <h1>Casa da Paz</h1>
+    <p>Links para Jogos Online</p>
+  </header>
+  <main class="container">
+    ${linksHtml}
+  </main>
+  <footer>
+    &copy; ${year} ${author}
+  </footer>
 </body>
 </html>
 `;
 
 export const getGameCard = ({ url, faviconUrl, nome, original }: GameCardProps): string => html`
 <a href="${url}" class="game-card" target="_blank">
-    <img src="${faviconUrl}" class="favicon" alt="">
-    <div class="info">
-        <h2>${nome}</h2>
-        <span>${original}</span>
-    </div>
+  <img src="${faviconUrl}" class="favicon" alt="">
+  <div class="info">
+    <h2>${nome}</h2>
+    <span>${original}</span>
+  </div>
 </a>
 `;
